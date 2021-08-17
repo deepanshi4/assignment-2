c2bf49e3d44feb178141efccf07e100b6e242407
deepanshi4 <79511955+deepanshi4@users.noreply.github.com>
2021-08-17 16:34:18 +0530

    positive no.py
    a Python program to print all positive numbers in a range

---
+++ positive no.py
@@ -0,0 +1,9 @@
+# assignment-2
+a Python program to print all positive numbers start, end = -4, 19
+# iterating each number in list
+for num in range(start, end + 1):
+
+    # checking condition
+    if num >= 0:
+        print(num, end = " ")in a range
+
